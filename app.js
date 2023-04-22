f648ead0c445660b9dbf5277daf7017c58b5a874
JUAN CARLOS ESCOBAR RODRIGUEZ <jcer@soy.sena.edu.co>
2023-04-22 16:55:49 -0500

    3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+
 //importacion de la libreria de exprres
 const express = require('express');
 const app = express();
-//se crean la rutas
-app.get('/',(req, res)=>{
-    res.send('Prueba 1 Respuestal del servidor');
-});
-
-app.listen(10000);
+// GET method route
+app.get('/', (req, res) => {
+    res.send('Prueba 1 Respuestal del servidor')
+  })
+  
+  app.listen(10000);
