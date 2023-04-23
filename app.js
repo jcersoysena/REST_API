cf2d3481ab0092117ba11a9b4dc380b928c51801
JUAN CARLOS ESCOBAR RODRIGUEZ <jcer@soy.sena.edu.co>
2023-04-22 19:51:55 -0500

    bd1

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 
 //importacion de la libreria de exprres
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+//const { default: mongoose } = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); 
 // GET method route
-app.get('/', (req, res) => {
+
+  app.use(bodyParser.json());
+  // importar las rutasw
+  const postRoute = require('./routes/post');
+  app.use('/servicios',postRoute);
+  
+
+  app.get('/', (req, res) => {
     res.send('Prueba 1 Respuestal del servidor');
   })
   //mongodb+srv://jcer:oP1WpyHTdXwOJRXk@mibd.7ss8add.mongodb.net/?retryWrites=true&w=majority
 
   //conexion a la BD
-  mongoose.connect(mongodb+srv://jcer:oP1WpyHTdXwOJRXk@mibd.7ss8add.mongodb.net/?retryWrites=true&w=majority,
+  mongoose.connect('mongodb+srv://jcer:oP1WpyHTdXwOJRXk@mibd.7ss8add.mongodb.net/?retryWrites=true&w=majority',
   {useNewUrlParser: true},() =>{
     console.log('si hay conexion a la BD')
   });
